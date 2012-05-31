1f56a159127d04bdd78993be354eaff5ab5ba76c
Laila Atrmouh <laila.atrmouh@gmail.com>
2012-05-31 17:43:48 +0200

    Test commit

--- src/agent/AgentBanque.java
+++ src/agent/AgentBanque.java
@@ -2,6 +2,11 @@ package agent;
 
 import jade.core.Agent;
 
+/**
+ * Agent banque
+ * @author leiluspocus
+ *
+ */
 public class AgentBanque extends Agent{
 	private static final long serialVersionUID = 1L;
 
