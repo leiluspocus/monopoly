9dd92cc8ecc01f17e2b0e36a35b45054cc312e48
Ronan <rkervell@etu.utc.fr>
2012-06-04 19:52:18 +0200

    Mise à jour des Behaviours d'intelligence joueur
    Création de la structure de base permettant de gérer les messages reçus
    par un joueur.

--- src/behaviour/AvideBehaviour.java
+++ src/behaviour/AvideBehaviour.java
@@ -2,6 +2,7 @@ package behaviour;
 
 import jade.core.Agent;
 import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
 import util.Logger;
 
 /**
@@ -16,8 +17,48 @@ public class AvideBehaviour extends OneShotBehaviour {
 	}
 
 	@Override
-	public void action() {
+	public void action() 
+	{
 		Logger.info("Action d'avide");
+		ACLMessage msgReceived = myAgent.receive();
+		
+		if (msgReceived != null)
+		{
+			switch (msgReceived.getPerformative()) 
+			{
+				/*
+				 * Demande de paiement: à destination d'un joueur (loyer/terrain/case spéciale), ou de la banque (taxes)
+				 */
+				case ACLMessage.REQUEST:
+					
+					break;
+				/*
+				 * Un joueur est sur une des propriétés de myAgent
+				 */
+				case ACLMessage.INFORM:
+					
+					break;
+				/*
+				 * Indique au joueur sur quelle case il se trouve après le déplacement effectué (dû au jeté de dés)
+				 */
+				case ACLMessage.INFORM_REF:
+					
+					break;
+				/*
+				 * Message receptionnant l'argent du joueur creditaire
+				 */
+				case ACLMessage.AGREE:
+					
+					break;
+					
+				default: Logger.info("Message non géré : "+msgReceived.getContent());
+					break;
+			}
+		}
+		else
+		{
+			block();
+		}
 	}
  
 }
--- src/behaviour/CollectionneurBehaviour.java
+++ src/behaviour/CollectionneurBehaviour.java
@@ -1,7 +1,9 @@
 package behaviour;
 
+import util.Logger;
 import jade.core.Agent;
 import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
 
 /**
  * Comportement visant à collectionner les terrains d'une même couleur
@@ -16,6 +18,46 @@ public class CollectionneurBehaviour extends OneShotBehaviour {
 
 	@Override
 	public void action() { 
+		
+ACLMessage msgReceived = myAgent.receive();
+		
+		if (msgReceived != null)
+		{
+			switch (msgReceived.getPerformative()) 
+			{
+				/*
+				 * Demande de paiement: à destination d'un joueur (loyer/terrain/case spéciale), ou de la banque (taxes)
+				 */
+				case ACLMessage.REQUEST:
+					
+					break;
+				/*
+				 * Un joueur est sur une des propriétés de myAgent
+				 */
+				case ACLMessage.INFORM:
+					
+					break;
+				/*
+				 * Indique au joueur sur quelle case il se trouve après le déplacement effectué (dû au jeté de dés)
+				 */
+				case ACLMessage.INFORM_REF:
+					
+					break;
+				/*
+				 * Message receptionnant l'argent du joueur creditaire
+				 */
+				case ACLMessage.AGREE:
+					
+					break;
+					
+				default: Logger.info("Message non géré : "+msgReceived.getContent());
+					break;
+			}
+		}
+		else
+		{
+			block();
+		}
 	} 
 
 }
