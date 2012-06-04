81e65a85b2f9fa4c055566ef9441a0373dad15df
Ronan <rkervell@etu.utc.fr>
2012-06-04 19:53:29 +0200

    Ajout de l'attribut propriétaireCase.
    Nécessité de connaître le propriétaire d'une case

--- src/view/Case.java
+++ src/view/Case.java
@@ -1,8 +1,10 @@
 package view;
 
+
 public abstract class Case{
 	protected int position; //Position de la case sur le plateau
 	protected String nomCase;  // Nom de la case
+	protected String proprietaireCase; // Agent propriétaire de la case (nom du joueur, défini dans AgentJoueur)
 	
 	public String getNom() { return nomCase; }
 	
