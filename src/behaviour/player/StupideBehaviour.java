dad6bd2f94216f6b0fd5e5d9eb4c016d94805cb1
lesnuages <ronan.kervella@gmail.com>
2012-06-07 23:18:28 +0200

    MAJ StupideBehaviour
    Mise à niveau du comportement

--- src/behaviour/player/StupideBehaviour.java
+++ src/behaviour/player/StupideBehaviour.java
@@ -3,7 +3,7 @@ package behaviour.player;
 import jade.core.Agent;
 import jade.core.behaviours.OneShotBehaviour;
 import jade.lang.acl.ACLMessage;
-import agent.AgentJoueur;
+import util.Logger;
 
 /**
  * Behaviour déclenché après tirage de dés
@@ -13,7 +13,7 @@ public class StupideBehaviour extends OneShotBehaviour {
 	
 	private static final long serialVersionUID = 1L;
 
-	private int probaDemandeLoyer = 50;
+	
 	
 	public StupideBehaviour(Agent agentJoueur) {
 		super(agentJoueur);
@@ -23,42 +23,23 @@ public class StupideBehaviour extends OneShotBehaviour {
 	public void action() {
 		
 		ACLMessage msgReceived = myAgent.blockingReceive();
-		
+
 		if (msgReceived != null)
 		{
 			switch (msgReceived.getPerformative()) 
 			{
-				/*
-				 * Demande de paiement: à destination d'un joueur (loyer/terrain/case spéciale), ou de la banque (taxes)
-				 * Envoi du paiement au destinataire
-				 */
-				case ACLMessage.REQUEST:
-					((AgentJoueur)myAgent).payerMontantDu(msgReceived);
-					break;
-				/*
-				 * Un joueur est sur une des propriétés de myAgent
-				 */
-				case ACLMessage.INFORM:
-					((AgentJoueur)myAgent).demanderLoyer(probaDemandeLoyer, msgReceived);
-					break;
 				/*
 				 * Indique au joueur sur quelle case il se trouve après le déplacement effectué (dû au jeté de dés)
 				 */
 				case ACLMessage.INFORM_REF:
 					//TODO: acheter la propriété si celle-ci est disponible
 					break;
-				/*
-				 * Message receptionnant l'argent d'un joueur ou de la banque
-				 */
-				case ACLMessage.AGREE:
-					int sommeRecue = Integer.parseInt(msgReceived.getContent().trim());
-					((AgentJoueur)myAgent).setCapitalJoueur(((AgentJoueur)myAgent).getCapitalJoueur()+sommeRecue);
-					break;
-					
-				default: System.out.println("Message non géré : "+msgReceived.getContent());
+	
+				default: 
+					Logger.info("Message non géré : "+msgReceived.getContent());
 					break;
 			}
-		} 
+		}
 	}
 
 
