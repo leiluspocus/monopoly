9650d01a365c44b8a55c83f7f753b3286ede17f9
rsezille <r.sezille@gmail.com>
2012-06-07 19:22:14 +0300

    Passage en mode JFrame

--- src/util/Logger.java
+++ src/util/Logger.java
@@ -1,14 +1,38 @@
 package util;
 
-public class Logger {
-
-	// Info classique liee l'action d'un joueur
-	public static void info(String msg) {
-		System.out.println(msg);
+public class Logger extends JFrame {
+	private JTextArea jta = new JTextArea();
+	
+	public LogFrame(){
+		super();
+		createGUI();
 	}
 	
-	// Erreur liee l'execution des agents
-	public static void err(String msg) {
-		System.err.println(msg);
+	private void createGUI(){
+		this.setTitle("Monopoly logs");
+		this.setSize(new Dimension(240, 660));
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.add(jta);
+		
+		this.setVisible(true);
+	}
+	
+	public void info(String line){
+		Pattern p = Pattern.compile("^.*\\\n+$");
+		Matcher m = p.matcher(line);
+		if(m.find())
+			jta.append(line);
+		else
+			jta.append(line+"\n");
+			
+		System.out.println(line);
+	}
+	
+	public void err(String line){
+		jta.append("<error>");
+		info(line);
+		System.err.println(line);
 	}
 }
